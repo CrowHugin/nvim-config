d89a471565b8b076d1e0a926e0ed9a5108ed0666
CrowHugin <kuntz.bastien@gmail.com>
2025-09-10 15:48:52 +0200

    changed options path

--- lua/config/keymaps.lua
+++
@@ -1,3 +0,0 @@
--- setting jj and jk to escape on normal mode
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
--- lua/options.lua
+++ lua/options.lua
@@ -3,3 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+
+-- setting jj and jk to escape on normal mode
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
