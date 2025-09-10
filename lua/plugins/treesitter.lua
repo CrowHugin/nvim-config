b024091bd123a2c719f45ee0d57c07ac2473943d
CrowHugin <kuntz.bastien@gmail.com>
2025-09-10 15:51:44 +0200

    remove some comments

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate", -- compile les parsers à l’installation/mise à jour
+  build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
       ensure_installed = {
         "lua", "vim", "vimdoc", "query", -- parsers essentiels pour Neovim
         "bash", "markdown", "markdown_inline", "json", "yaml", "html", "css", "javascript","c","python","lua",
       },
-      sync_install = false, -- installation asynchrone (plus rapide)
-      auto_install = true,  -- installe automatiquement les parsers manquants quand tu ouvres un fichier
+      sync_install = false, 
+      auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
