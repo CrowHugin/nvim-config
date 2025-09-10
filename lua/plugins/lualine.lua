f6af13726601f834ed0111de52e46749bdd63cb0
CrowHugin <kuntz.bastien@gmail.com>
2025-09-10 19:18:06 +0200

    add time and day to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,21 @@
 return{
   'nvim-lualine/lualine.nvim',
+  dependencies = {
+    "archibate/lualine-time",
+    },
   config = function()
     require("lualine").setup({
       options = {
         theme = 'dracula'
-      }
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'ctime','cdate'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'},
+      },
     })
   end
 }
