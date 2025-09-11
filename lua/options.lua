c5add960e1b4b5e31368b9f2a9e1fa099ea84b8f
CrowHugin <kuntz.bastien@gmail.com>
2025-09-11 17:37:03 +0200

    Changed keymap to switch windows with tab and shift tab

--- lua/options.lua
+++ lua/options.lua
@@ -7,3 +7,9 @@ vim.cmd("set number")
 -- setting jj and jk to escape on normal mode
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+
+-- Aller à la fenêtre suivante avec Tab
+vim.keymap.set('n', '<Tab>', '<C-w>w', { noremap = true, silent = true })
+-- Aller à la fenêtre précédente avec Shift-Tab
+vim.keymap.set('n', '<S-Tab>', '<C-w>W', { noremap = true, silent = true })
+
