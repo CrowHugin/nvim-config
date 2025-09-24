30c641538c92cb19d87a8fd2abfe5c7d9b5c9347
CrowHugin <kuntz.bastien@gmail.com>
2025-09-24 10:57:09 +0200

    Add option for settings & some desc

--- lua/plugins/nvim-neo-tree.lua
+++ lua/plugins/nvim-neo-tree.lua
@@ -8,6 +8,7 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
-    vim.keymap.set("n", "<leader>n", ":Neotree toggle filesystem reveal right<CR>")
+    vim.keymap.set("n", "<leader>n", ":Neotree toggle filesystem reveal right<CR>",{desc = "Toggle the filesystem tree on right"}),
+    vim.keymap.set("n","<Leader>cp",":e $MYVIMRC | :cd %:p:h | :Neotree toggle filesystem reveal right<CR>",{desc = "Go to setting"}),
   }
 }
