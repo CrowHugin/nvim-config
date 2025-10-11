6f4d922ad88e3fbe8662cee25fb14131838234b0
CrowHugin <kuntz.bastien@gmail.com>
2025-10-11 14:58:17 +0200

    Removed arrows' keymap

--- lua/options.lua
+++ lua/options.lua
@@ -13,3 +13,12 @@ vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
 vim.keymap.set('n', '<Tab>', '<C-w>w', { noremap = true, silent = true })
 -- Aller à la fenêtre précédente avec Shift-Tab
 vim.keymap.set('n', '<S-Tab>', '<C-w>W', { noremap = true, silent = true })
+
+-- disabla arrows to ensure the use of h j k l
+for _, mode in ipairs({ 'n', 'i', 'v' }) do
+  vim.keymap.set(mode, '<Up>', '<Nop>', { noremap = true, silent = true })
+  vim.keymap.set(mode, '<Down>', '<Nop>', { noremap = true, silent = true })
+  vim.keymap.set(mode, '<Left>', '<Nop>', { noremap = true, silent = true })
+  vim.keymap.set(mode, '<Right>', '<Nop>', { noremap = true, silent = true })
+end
+
