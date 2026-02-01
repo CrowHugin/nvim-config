4be517cca8f964dc2d0a23c0b9756ba94f6eafdb
CrowHugin <kuntz.bastien@gmail.com>
2026-02-01 18:39:42 +0100

    Updated treesitter plugin for better compatibility

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
@@ -7,8 +8,8 @@ return {
         "lua", "vim", "vimdoc", "query", -- parsers essentiels pour Neovim
         "bash", "markdown", "markdown_inline", "json", "yaml", "html", "css", "javascript","c","python","lua",
       },
-      sync_install = false, 
-      auto_install = true,
+      sync_install = false,
+      auto_install = false,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
