0ae925b1b1f8484e0d46c420cd4720f75ae957a4
CrowHugin <kuntz.bastien@gmail.com>
2026-04-18 16:31:50 +0200

    Updated some plugins && add mini_pairs plugin

---
+++ lua/plugins/mini_pairs.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-mini/mini.pairs",
+  event = "InsertEnter",
+  opts = {
+    -- Les modes dans lesquels les paires sont actives
+    modes = { insert = true, command = false, terminal = false },
+    -- Configuration des paires par défaut
+    mappings = {
+      ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
+      ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
+      ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+
+      [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+      [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+      ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+
+      ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
+      ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^\\].', register = { cr = false } },
+      ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
+    },
+  },
+  config = function(_, opts)
+    require("mini.pairs").setup(opts)
+  end,
+}
